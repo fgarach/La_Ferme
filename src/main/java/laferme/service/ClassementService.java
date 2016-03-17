c35be59e0b498d3381d845bbd4bbea2b1a4f0987
fgarach <garach.fanny@yahoo.fr>
2016-03-17 16:06:28 +0100

    classement debut

---
+++ src/main/java/laferme/service/ClassementService.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laferme.service;
+
+import java.util.List;
+import laferme.configuration.Config;
+import laferme.entity.Ressource;
+import laferme.entity.Utilisateur;
+import laferme.enumeration.TypeEtat;
+import laferme.enumeration.TypeRessource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author admin
+ */
+@Service
+public class ClassementService {
+
+    @Autowired
+    RessourceCrudService ressourceCrudService;
+
+    @Autowired
+    UtilisateurCrudService utilisateurCrudService;
+
+    public List<Utilisateur> classer() {
+
+        List<Utilisateur> utilisateurs = (List<Utilisateur>) utilisateurCrudService.findAll();
+
+        for (Utilisateur util : utilisateurs) {
+
+            List<Ressource> carottesDispo = ressourceCrudService.findByTypeRessourceAndTypeEtat(TypeRessource.CAROTTE, TypeEtat.VIVANT);
+            List<Ressource> carottePlantees = ressourceCrudService.findByTypeRessourceAndTypeEtat(TypeRessource.CAROTTE, TypeEtat.OCCUPE);
+            List<Ressource> blesDispo = ressourceCrudService.findByTypeRessourceAndTypeEtat(TypeRessource.BLE, TypeEtat.VIVANT);
+            List<Ressource> blePlantes = ressourceCrudService.findByTypeRessourceAndTypeEtat(TypeRessource.BLE, TypeEtat.OCCUPE);
+            List<Ressource> chevresNonEnceintes = ressourceCrudService.findByTypeRessourceAndTypeEtat(TypeRessource.CHEVRE, TypeEtat.VIVANT);
+            List<Ressource> chevresEnceintes = ressourceCrudService.findByTypeRessourceAndTypeEtat(TypeRessource.CHEVRE, TypeEtat.OCCUPE);
+
+            int nbCarotte = carottesDispo.size() + carottePlantees.size();
+            int nbChevre = chevresNonEnceintes.size() + chevresEnceintes.size();
+            int nbBle = blesDispo.size() + blePlantes.size();
+
+            int score = nbCarotte * Config.tauxEchangeCarotte + nbChevre * Config.tauxEchangeChevre + nbBle * Config.tauxEchangeBle;
+
+        }
+        return utilisateurs;
+
+    }
+
+}
--- src/main/java/laferme/servlet/ClassementServlet.java
+++ src/main/java/laferme/servlet/ClassementServlet.java
@@ -13,9 +13,12 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import laferme.configuration.Config;
 import laferme.entity.Ressource;
+import laferme.entity.Utilisateur;
 import laferme.enumeration.TypeEtat;
 import laferme.enumeration.TypeRessource;
+import laferme.service.ClassementService;
 import laferme.service.RessourceCrudService;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -28,27 +31,16 @@ public class ClassementServlet extends HttpServlet {
 
     @Autowired
     RessourceCrudService ressourceCrudService;
+    
+    @Autowired
+    ClassementService classementService;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String option = "classement";
         req.setAttribute("option", option);
-        
-        List<Ressource> carottesDispo = ressourceCrudService.findByTypeRessourceAndTypeEtat(TypeRessource.CAROTTE, TypeEtat.VIVANT);
-        List<Ressource> blesDispo = ressourceCrudService.findByTypeRessourceAndTypeEtat(TypeRessource.BLE, TypeEtat.VIVANT);
-        List<Ressource> fromagesDispo = ressourceCrudService.findByTypeRessourceAndTypeEtat(TypeRessource.FROMAGE, TypeEtat.VIVANT);
-
-        List<Ressource> carottePlantees = ressourceCrudService.findByTypeRessourceAndTypeEtat(TypeRessource.CAROTTE, TypeEtat.OCCUPE);
-        List<Ressource> blePlantes = ressourceCrudService.findByTypeRessourceAndTypeEtat(TypeRessource.BLE, TypeEtat.OCCUPE);
-        List<Ressource> chevresNonEnceintes = ressourceCrudService.findByTypeRessourceAndTypeEtat(TypeRessource.CHEVRE, TypeEtat.VIVANT);
-        List<Ressource> chevresEnceintes = ressourceCrudService.findByTypeRessourceAndTypeEtat(TypeRessource.CHEVRE, TypeEtat.OCCUPE);
-        
-        int nbCarotte = carottesDispo.size()+carottePlantees.size();
-        int nbFromage = fromagesDispo.size();
-        int nbChevre = chevresNonEnceintes.size()+chevresEnceintes.size();
-        int nbBle = blesDispo.size()+blePlantes.size();
-        
-//        int score = nbCarotte
+        List <Utilisateur> classements = classementService.classer();
+        req.setAttribute("classement", classements);
                 
         req.getRequestDispatcher("plateforme").forward(req, resp);
     }
